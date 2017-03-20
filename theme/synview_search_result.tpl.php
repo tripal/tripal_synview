223677766cb4d590d4169bd32f4bca7108bd6f6d
kentnf <kentnf@gmail.com>
2017-03-20 16:26:38 -0400

    update: display overlap with same bgcolor; fix error when reload page; other bugs

--- theme/synview_search_result.tpl.php
+++ theme/synview_search_result.tpl.php
@@ -11,19 +11,36 @@ else {
   $rows = array();
   $headers = array('Block' , 'Organism1 (location)', 'Organism2 (location)', 'score', 'evalue');
 
-  foreach ($blocks as $b) {
-    $block_id = $b->blockid;
-    $block_id = l($block_id, "synview/block/". $block_id, array('attributes' => array('target' => "_blank")));
-    $organism1 = $b->b1_org . "<br>" . $b->b1_sid . " : ". $b->b1_fmin . " - ".$b->b1_fmax;
-    $organism2 = $b->b2_org . "<br>" . $b->b2_sid . " : ". $b->b2_fmin . " - ".$b->b2_fmax;
+  $n = 0;
+  $color = '#DEEEEE';
 
-    $rows[] = array(
-      array('data'=> $block_id, 'width' => '10%'),
-      array('data'=> $organism1, 'width' => '20%'),
-      array('data'=> $organism2, 'width' => '20%'),
-      array('data'=> $b->score, 'width' => '10%'),
-      array('data'=> $b->evalue, 'width' => '10%'),
-    );
+  foreach ($cluster as $cls) {
+    $n++;
+    foreach ($cls as $bid) {
+      $b = $blocks[$bid];
+      $block_id = $b->blockid;
+      $block_id = l($block_id, "synview/block/". $block_id, array('attributes' => array('target' => "_blank")));
+      $organism1 = $b->b1_org . "<br>" . $b->b1_sid . " : ". $b->b1_fmin . " - ".$b->b1_fmax;
+      $organism2 = $b->b2_org . "<br>" . $b->b2_sid . " : ". $b->b2_fmin . " - ".$b->b2_fmax;
+ 
+      if ($n % 2 == 0) {
+        $rows[] = array(
+          array('data'=> $block_id,  'width' => '10%', 'bgcolor' => $color),
+          array('data'=> $organism1, 'width' => '20%', 'bgcolor' => $color),
+          array('data'=> $organism2, 'width' => '20%', 'bgcolor' => $color),
+          array('data'=> $b->score,  'width' => '10%', 'bgcolor' => $color),
+          array('data'=> $b->evalue, 'width' => '10%', 'bgcolor' => $color),
+        );
+      } else {
+        $rows[] = array(  
+          array('data'=> $block_id, 'width' => '10%'),
+          array('data'=> $organism1, 'width' => '20%'),
+          array('data'=> $organism2, 'width' => '20%'),
+          array('data'=> $b->score, 'width' => '10%'),
+          array('data'=> $b->evalue, 'width' => '10%'),
+        );
+      }
+    }
   }
 
   $table = array(
